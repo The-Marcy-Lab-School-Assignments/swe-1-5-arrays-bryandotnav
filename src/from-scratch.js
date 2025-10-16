888209cb7272ed8b9465fa5f159cc92dc6883281
Bryan <bryan001700@gmail.com>
2025-10-15 23:50:53 -0400

    completed all but one.

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,56 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if(arr.length === 0){
+    arr.push(value)
+    return arr;
+  };
+  if(value < arr[0]){
+      arr.unshift(value)
+      return arr
+    } else if(value>arr[arr.length-1]){
+      arr.push(value)
+      return arr
+    }
+  return arr;
 };
 
-const reverseString = () => {
+const reverseString = (str) => {
+  const string = str.split("");
+  const reversedString = string.reverse();
+  const reversedStr = reversedString.join("")
+  return reversedStr;
 };
 
-const newArrayFullOf = () => {
+const newArrayFullOf = (value, numOfValue) => {
+  const newArr = arr.splice(0, arr.length, value)
 };
 
-const insertIntoMiddle = () => {
+const insertIntoMiddle = (arr, num) => {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 0, num);
+  console.log(arr);
 };
 
-const deleteFromMiddle = () => {
+const deleteFromMiddle = (arr) => {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 1);
+  console.log(arr);
 };
 
-const isRightIndex = () => {
+const isRightIndex = (arr, value, index) => {
+ return arr[index] === value;
 };
 
-const roundAllNumsDown = () => {
+const roundAllNumsDown = (arr) => {
+  roundedArr = arr.map(num => Math.floor(num));
+  return roundedArr;
 };
 
-const getAllYCoordinates = () => {
+const getAllYCoordinates = (arr) => {
+  newArr = [];
+  for(let i=0;i<arr.length;i++){
+    newArr.push(arr[i][1]);
+  }
+  return newArr;
 };
 
 module.exports = {
--- src/playground.js
+++ src/playground.js
@@ -1,12 +1,17 @@
 // const { clearArr, getFirstItem } = require('./debug');
-// const {
+ const {
 //   addToFrontOrBack,
-//   reverseString,
+   reverseString,
 //   newArrayFullOf,
-//   insertIntoMiddle,
-//   deleteFromMiddle,
-//   isRightIndex,
-//   roundAllNumsDown,
+   insertIntoMiddle,
+   deleteFromMiddle,
+   isRightIndex,
+  roundAllNumsDown,
 //   getAllYCoordinates,
-// } = require('./from-scratch');
+ } = require('./from-scratch');
 // const { uppercaseAll, destructureCoordinates } = require('./modify');
+console.log(reverseString("hello"));
+insertIntoMiddle([2,4,8,9], 5);
+deleteFromMiddle([2,4,7,8]);
+console.log(isRightIndex(['a','s','f'], 's', 2));
+console.log(roundAllNumsDown([1.1, 2.2, 3.3]));
